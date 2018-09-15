88d643304ab395719bf64633de35cf6278e05c2f
shilpi singh <shilpi@qwickbit.com>
2018-09-15 11:36:24 +0530

    fix for auth token

--- src/app/auth/authentication.service.ts
+++ src/app/auth/authentication.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
 import { environment } from '../../environments/environment';
+import { tokenNotExpired } from 'angular2-jwt';
 
 @Injectable()
 export class AuthenticationService {
