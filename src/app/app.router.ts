fdb03204985cde7cbc810f7f4c02321c30e64b45
vivek <v4vivek30@gmail.com>
2018-09-22 12:14:03 +0530

    .

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -4,7 +4,7 @@ import { ModuleWithProviders } from '@angular/core';
 
 
 export const appRouter: Routes = [
-    { path: '', redirectTo: 'auth', pathMatch: 'full' },
+    { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
     { path: 'auth', loadChildren: './auth/auth.module#AuthModule' },
     { path: 'dashboard', loadChildren:  './dashboard/dashboard.module#DashboardModule', canActivate: [AuthGuard] }
     
