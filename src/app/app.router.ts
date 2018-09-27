cca2a684eda5f263f17152cf62931b93e8256957
Anand Kadiveti <anand.kadiveti@gmail.com>
2018-09-27 11:22:08 +0530

    routing fix

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -4,7 +4,7 @@ import { ModuleWithProviders } from '@angular/core';
 
 
 export const appRouter: Routes = [
-    { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+    { path: '', redirectTo: 'auth', pathMatch: 'full' },
     { path: 'auth', loadChildren: './auth/auth.module#AuthModule' },
     { path: 'dashboard', loadChildren:  './dashboard/dashboard.module#DashboardModule', canActivate: [AuthGuard] }
     
