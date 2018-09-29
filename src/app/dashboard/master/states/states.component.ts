8380553fa25d350fd86011e3ba531190a1a266ad
Anand Kadiveti <anand.kadiveti@gmail.com>
2018-09-29 17:29:57 +0530

    States functionality

--- src/app/dashboard/master/states/states.component.ts
+++ src/app/dashboard/master/states/states.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { TranslateService } from '@ngx-translate/core';
+import { MasterService } from '../master.service';
+
 
 @Component({
   selector: 'app-states',
@@ -7,9 +11,119 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StatesComponent implements OnInit {
 
-  constructor() { }
+  public statesForm: FormGroup;  
+  public formSuccess: boolean = false;
+  public formRequiredError: boolean = false;
+  public formServerError: boolean = false;
+  public statesList: any = [];
+  public statesListColumns;
+  public editStates;
+  public deleteFlag;
+  public nameFlag;
 
-  ngOnInit() {
+ 
+  @ViewChild('focus') focusField: ElementRef;
+
+  constructor(private fb: FormBuilder, private translate: TranslateService, private masterService: MasterService) {
+    translate.setDefaultLang('messages.en');
+    
   }
 
+  valueChange(selectedRow: any[]): void {
+    this.editable(selectedRow);
+  }
+
+  ngOnInit() {
+    this.statesForm = this.fb.group({
+      id: [],
+      stateName: ['', Validators.required],
+      stateCode: ['', Validators.required],
+      zone: ['', Validators.required],
+    });
+
+    this.loadGridData();
+    this.focusField.nativeElement.focus(); 
+    this.getStatesTypes();   
+  }
+
+  loadGridData() {
+    this.masterService.getData("states/");
+    this.masterService.dataObject.subscribe(list => {
+      this.statesList = list;
+      localStorage.setItem('rowDataLength', JSON.stringify(this.statesList.length));
+    });
+  }
+
+  getStatesTypes() {
+    this.masterService.getLocalJsonData().subscribe(data => {
+       data as object [];        
+        this.statesListColumns = data["StateListColumns"]
+    });
+  }
+
+  save() {
+    if (this.statesForm.valid) {
+      if (confirm('Are you sure!!')) {
+        if (this.statesForm.value.id) {
+          this.masterService.updateRecord('states/', this.statesForm.value).subscribe(res => {
+            this.successMsg();
+          }, (error) => {
+            this.serverErrMsg();
+          });
+        } else {
+          this.masterService.createRecord('states/', this.statesForm.value).subscribe(res => {
+            this.successMsg();
+          }, (error) => {
+            this.serverErrMsg();
+          });
+        }
+      }
+    } else {
+      this.requiredErrMsg()
+    }
+  }
+
+  delete() {
+    if (confirm('Are you sure!!')) {
+      this.masterService.deleteRecord('states/', this.editStates.id).subscribe(res => {
+        localStorage.removeItem('ag-activeRow');
+        this.successMsg()
+      }, (error) => {
+        this.serverErrMsg();
+      });
+    }
+  }
+
+  successMsg() {
+    this.formSuccess = true;
+    this.formRequiredError = this.formServerError = false;
+    this.resetForm();
+  }
+
+  requiredErrMsg() {
+    this.formRequiredError = true;
+    this.formSuccess = this.formServerError = false;
+  }
+
+  serverErrMsg() {
+    this.formServerError = true;
+    this.formRequiredError = this.formSuccess = false;
+  }
+
+  resetForm() {
+    this.statesForm.reset();
+    this.editStates = null;
+    this.deleteFlag = true;
+    this.nameFlag = false;
+    this.formRequiredError = this.formServerError = this.formSuccess = false;
+    this.loadGridData();
+    this.focusField.nativeElement.focus();
+  }
+
+  editable(s) {
+    this.editStates = s;
+    this.statesForm.reset(s);
+    this.deleteFlag = !this.editStates.deleteFlag;
+    this.nameFlag = true;
+  }
 }
