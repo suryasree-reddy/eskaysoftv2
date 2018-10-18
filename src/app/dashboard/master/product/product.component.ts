61c85dde951f9650bda8cc380485b6b80e15c975
Samanth <samanth2012sam@gmail.com>
2018-10-18 17:46:35 +0530

    Product Form Modals

--- src/app/dashboard/master/product/product.component.ts
+++ src/app/dashboard/master/product/product.component.ts
@@ -15,9 +15,14 @@ import * as _ from 'lodash';
 export class ProductComponent implements OnInit {
 
   public productForm: FormGroup;
-  public productGroupName: FormGroup;
+ // public productGroupName: FormGroup;
+  public productGroupForm: FormGroup;
   public productCategoryForm: FormGroup;
+  public companyForm: FormGroup;
   private endPoint: string = "product/";
+  private pgEndPoint: string = "productgroup/";
+  private pcEndPoint: string = "productcategory/";
+  private cEndPoint: string = "company/";
   public gridDataList: any = [];
   public gridColumnNamesList;
   public gridSelectedRow;
@@ -27,20 +32,29 @@ export class ProductComponent implements OnInit {
   public nameFlag;
   public deleteFlag: boolean = true;
   public duplicateMessage: string = null;
+  public duplicateMessageParam: string = null;
+  public scFormRequiredError: boolean = false;
+  public scFormServerError: boolean = false;
+  public scFormSuccess: boolean = false;
+  public childDuplicateMessage: string = null;
+  public childDuplicateMessageParam: string = null;
+  private duplicateProdGroup: boolean = false;
+  private duplicateProdCategory: boolean = false;
+  private duplicateCompany: boolean = false;
   public typeaheadGroupDataList: any = [];
   public typeaheadCompanyDataList: any = [];
   public typeaheadCategoryDataList: any = [];
   public selectedCategoryTypeahead: any;
   public selectedGroupTypeahead: any;
   public selectedCompanyTypeahead: any;
-  private pgEndPoint: string = "productgroup/";
-  private pcEndPoint: string = "productcategory/";
-  private cEndPoint: string = "company/";
 
+  modalRef: BsModalRef;
+  message: string;
 
   @ViewChild('focus') focusField: ElementRef;
 
-  constructor(private fb: FormBuilder, private translate: TranslateService,
+  constructor(private fb: FormBuilder, 
+    private translate: TranslateService,
     private modalService: BsModalService,
     private masterService: MasterService) {
     translate.setDefaultLang('messages.en');
@@ -56,6 +70,7 @@ export class ProductComponent implements OnInit {
       packing: ['', Validators.required],
       boxQty: ['', Validators.required],
       productGroupName: ['', Validators.required],
+      companyCode: ['', Validators.required],
       caseQty: ['', Validators.required],
       productCategoryName: ['', Validators.required],
       netRate: ['', Validators.required],
@@ -66,6 +81,22 @@ export class ProductComponent implements OnInit {
       tax: ['', Validators.required],
       code: ['', Validators.required]
     });
+
+    this.productGroupForm = this.fb.group({
+      id: [],
+      productGroupName: ['', Validators.required]
+    });
+
+    this.productCategoryForm = this.fb.group({
+      id: [],
+      productCategoryName: ['', Validators.required]
+    });
+
+    this.companyForm = this.fb.group({
+      companyId: [],
+      companyCode: ['', Validators.required]
+    });
+    
     //this.loadGridData();
     this.getGridCloumsList();
     this.loadGroupTypeaheadData();
@@ -123,6 +154,53 @@ loadCompanyTypeaheadData(){
     this.gridDataList = dataList;
   }
 
+  openModal(template: TemplateRef<any>) {
+    this.resetChildForm();
+    this.scFormRequiredError = this.scFormServerError = this.scFormSuccess = false;
+    this.modalRef = this.modalService.show(template, { class: 'modal-md' });
+  }
+
+  getDuplicateErrorMessages(): void {
+    this.duplicateMessage = null;
+    this.childDuplicateMessage = null;
+    this.childDuplicateMessageParam = null;
+    this.formRequiredError = false;
+    this.duplicateMessageParam = null;
+    this.scFormRequiredError = false;
+
+      if (this.duplicateProdGroup) {
+      this.childDuplicateMessage = "productgroup.duplicateNameErrorMessage";
+      this.childDuplicateMessageParam = this.productGroupForm.value.productGroupName;
+    }
+    else if (this.duplicateProdCategory) {
+      this.childDuplicateMessage = "productcategory.duplicateNameErrorMessage";
+      this.childDuplicateMessageParam = this.productCategoryForm.value.productCategoryName;
+    }
+    else if (this.duplicateCompany) {
+      this.childDuplicateMessage = "companies.duplicateNameErrorMessage";
+      this.childDuplicateMessageParam = this.companyForm.value.companyCode;
+    }
+  }
+
+  checkForDuplicateProdGroup() {
+    if (!this.nameFlag) {
+      this.duplicateProdGroup = this.masterService.hasDataExist(this.typeaheadGroupDataList, 'productGroupName', this.productGroupForm.value.productGroupName);
+      this.getDuplicateErrorMessages();
+    }
+  }
+
+  checkForDuplicateProdCategory() {
+    if (!this.nameFlag) {
+      this.duplicateProdCategory = this.masterService.hasDataExist(this.typeaheadCategoryDataList, 'productCategoryName', this.productCategoryForm.value.productCategoryName);
+      this.getDuplicateErrorMessages();
+    }
+  }
+
+  checkForDuplicateCompanyCode() {
+    this.duplicateCompany = this.masterService.hasDataExist(this.gridDataList, 'companyCode', this.companyForm.value.companyCode);
+    this.getDuplicateErrorMessages();
+}
+
   getGridCloumsList() {
     this.masterService.getLocalJsonData().subscribe(data => {
       data as object[];
@@ -163,6 +241,64 @@ loadCompanyTypeaheadData(){
     }
   }
 
+  
+  saveChildForm() {
+    this.scFormRequiredError = false;
+    if (this.productGroupForm.valid && this.childDuplicateMessage == null) {
+      this.showConfirmationModal("SaveChildForm");
+    } else {
+      this.scRequiredErrMsg()
+    }
+  }
+
+  saveChildData() {
+    this.masterService.createRecord(this.pgEndPoint, this.productGroupForm.value).subscribe(res => {
+      this.showInformationModal("SaveChildForm");
+
+    }, (error) => {
+      this.serverErrMsg();
+    });
+
+  }
+
+  saveChildPCForm() {
+    this.scFormRequiredError = false;
+    if (this.productCategoryForm.valid && this.childDuplicateMessage == null) {
+      this.showConfirmationModal("SaveChildPCForm");
+    } else {
+      this.scRequiredErrMsg()
+    }
+  }
+
+  saveChildPCData() {
+    this.masterService.createRecord(this.pcEndPoint, this.productCategoryForm.value).subscribe(res => {
+      this.showInformationModal("SaveChildPCForm");
+
+    }, (error) => {
+      this.serverErrMsg();
+    });
+
+  }
+
+  saveChildCmpForm() {
+    this.scFormRequiredError = false;
+    if (this.companyForm.valid && this.childDuplicateMessage == null) {
+      this.showConfirmationModal("SaveChildCmpForm");
+    } else {
+      this.scRequiredErrMsg()
+    }
+  }
+
+  saveChildCmpData() {
+    this.masterService.createRecord(this.cEndPoint, this.companyForm.value).subscribe(res => {
+      this.showInformationModal("SaveChildCmpForm");
+
+    }, (error) => {
+      this.serverErrMsg();
+    });
+
+  }
+
   delete() {
     if (confirm('Are you sure!!')) {
       this.masterService.deleteRecord(this.endPoint, this.gridSelectedRow.id).subscribe(res => {
@@ -178,6 +314,7 @@ loadCompanyTypeaheadData(){
     this.formSuccess = true;
     this.formRequiredError = this.formServerError = false;
     this.resetForm();
+    this.resetChildForm();
   }
 
   requiredErrMsg() {
@@ -211,6 +348,24 @@ loadCompanyTypeaheadData(){
     this.deleteFlag = false;
   }
 
+  resetChildForm() {
+    this.scFormRequiredError = false;
+    this.scFormServerError = false;
+    this.duplicateMessage = null;
+    this.childDuplicateMessage = null;
+    this.childDuplicateMessageParam = null;
+    this.formRequiredError = false;
+    this.duplicateMessageParam = null;
+    this.productGroupForm.reset();
+    this.productCategoryForm.reset();
+    this.companyForm.reset();
+  }
+
+  scRequiredErrMsg() {
+    this.scFormRequiredError = true;
+    this.scFormSuccess = this.scFormServerError = false;
+  }
+
   showInformationModal(eventType) {
     var msg;
     var title;
@@ -221,6 +376,22 @@ loadCompanyTypeaheadData(){
       title = 'Product';
       msg = 'product.saveInformationMessage';
     }
+    else if (eventType === "SaveChildForm") {
+      title = 'Product Group';
+      msg = 'productgroup.saveInformationMessage';
+    }
+    else if (eventType === "SaveChildPCForm") {
+      title = 'Product Category';
+      msg = 'productcategory.saveInformationMessage';
+
+    }
+    else if (eventType === "SaveChildCmpForm") {
+      title = 'Company';
+      msg = 'companies.saveInformationMessage';
+
+    }
+
+
     const modal = this.modalService.show(ConfirmationModelDialogComponent);
     (<ConfirmationModelDialogComponent>modal.content).showInformationModal(
       title,
@@ -240,6 +411,18 @@ loadCompanyTypeaheadData(){
       title = 'Product';
       msg = 'product.saveConfirmationMessage';
     }
+    else if (eventType === "SaveChildForm") {
+      title = 'Product Group';
+      msg = 'productgroup.saveConfirmationMessage';
+    }
+    else if (eventType === "SaveChildPCForm") {
+      title = 'Product Category';
+      msg = 'productcategory.saveConfirmationMessage';
+    }
+    else if (eventType === "SaveChildCmpForm") {
+      title = 'Company';
+      msg = 'companies.saveConfirmationMessage';
+    }
     const modal = this.modalService.show(ConfirmationModelDialogComponent);
     (<ConfirmationModelDialogComponent>modal.content).showConfirmationModal(
       title,
@@ -252,7 +435,16 @@ loadCompanyTypeaheadData(){
       if (result) {
         if (eventType === "Delete") {
           this.delete();
-        } else {
+        } else if (eventType === "SaveChildForm") {
+          this.saveChildData();
+        }
+        else if (eventType === "SaveChildPCForm") {
+          this.saveChildPCData();
+        }
+        else if (eventType === "SaveChildCmpForm") {
+          this.saveChildPCData();
+        }
+        else {
           this.save();
         }
       }
