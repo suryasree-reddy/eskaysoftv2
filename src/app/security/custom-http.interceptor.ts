11b63e3fdf0318052bbb3324d248ed6f1c91d42c
Vijayasai <VijayaSai.Z.Kesanupalli@td.com>
2018-11-06 18:11:35 -0500

    changes

--- src/app/security/custom-http.interceptor.ts
+++ src/app/security/custom-http.interceptor.ts
@@ -1,14 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { SharedDataService } from 'src/app/shared/model/shared-data.service';
 
 @Injectable()
 export class CustomHttpInterceptor implements HttpInterceptor {
-    constructor() {}
+    constructor(private sharedDataService:SharedDataService) {}
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+      this.sharedDataService.setErrorCode(null);
+      this.sharedDataService.setErrorMesage(null);
        if(localStorage.getItem('id_token')){
-            const token = JSON.parse(localStorage.getItem('id_token'));                
+            const token = JSON.parse(localStorage.getItem('id_token'));
             if(token && token.tokenType){
                 const headers = req.headers.set('Authorization', token.tokenType + ' ' + token.accessToken );
                 const authReq = req.clone({ headers });
