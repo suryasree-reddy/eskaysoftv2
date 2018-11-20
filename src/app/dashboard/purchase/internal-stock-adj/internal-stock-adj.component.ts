3bf64f72ec29f34b205229c10f71ff20551309f8
Vijayasai <VijayaSai.Z.Kesanupalli@td.com>
2018-11-19 20:24:24 -0500

    purchase screens

--- src/app/dashboard/purchase/internal-stock-adj/internal-stock-adj.component.ts
+++ src/app/dashboard/purchase/internal-stock-adj/internal-stock-adj.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MasterService } from 'src/app/dashboard/master/master.service';
+import { TranslateService } from '@ngx-translate/core';
+import { ButtonsComponent } from 'src/app/commonComponents/buttons/buttons.component';
+import { SharedDataService } from 'src/app/shared/model/shared-data.service';
 
 @Component({
   selector: 'app-internal-stock-adj',
@@ -7,9 +12,82 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InternalStockAdjComponent implements OnInit {
 
-  constructor() { }
+  private internalStockForm: FormGroup;
+  private deleteFlag: boolean = true;
+  private endPoint: string = "createUser/";
+  private formSuccess: boolean = false;
+  private formRequiredError: boolean = false;
+  private nameFlag: boolean = false;
+  private duplicateName: boolean = false;
+  private duplicateUserName: boolean = false;
+  private duplicateMessage: string = null;
+  private duplicateMessageParam: string = null;
+  private internalStockList: any = [];
+
+  @ViewChild(ButtonsComponent) buttonsComponent: ButtonsComponent;
+
+  constructor(private fb: FormBuilder,
+    private translate: TranslateService,
+    private sharedDataService: SharedDataService,
+    private masterService: MasterService) {
+    translate.setDefaultLang('messages.en');
+
+  }
 
   ngOnInit() {
+    this.internalStockForm = this.fb.group({
+      id: ['', Validators.required]
+    });
+
+  //  this.rolesList = this.sharedDataService.getSharedCommonJsonData().UserRoles;
+  }
+
+  checkForDuplicateName() {
+
+  }
+
+  checkForDuplicateUserName() {
+
+  }
+
+  getDuplicateErrorMessages(): void {
+    if (!this.duplicateName || !this.duplicateUserName) {
+      this.formRequiredError = false;
+      this.duplicateMessage = null;
+      this.duplicateMessageParam = null;
+    }
+  }
+
+  save() {
+    this.buttonsComponent.save();
+  }
+
+  delete() {
+    this.buttonsComponent.delete();
+  }
+
+  successMsg() {
+    this.formSuccess = true;
+    this.formRequiredError = false;
+    this.resetForm();
+  }
+
+  requiredErrMsg() {
+    if (this.duplicateMessage == null) {
+      this.formRequiredError = true;
+      this.formSuccess = false;
+    }
+  }
+
+  resetForm() {
+    this.internalStockForm.reset();
+    this.deleteFlag = true;
+    this.duplicateMessage = null;
+    this.duplicateMessageParam = null;
+    this.nameFlag = false;
+    this.duplicateName = false;
+    this.duplicateUserName = false;
+    this.formRequiredError = this.formSuccess = false;
   }
 
 }
