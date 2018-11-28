d025c4c2f3627cc78b43cf3bcd6da8f85e528c34
Samanth <samanth2012sam@gmail.com>
2018-11-28 20:51:19 +0530

    Reports & Changes

--- src/app/dashboard/purchase/reports/reports.component.ts
+++ src/app/dashboard/purchase/reports/reports.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MasterService } from 'src/app/dashboard/master/master.service';
+import { TranslateService } from '@ngx-translate/core';
+import { ButtonsComponent } from 'src/app/commonComponents/buttons/buttons.component';
+import { SharedDataService } from 'src/app/shared/model/shared-data.service';
 
 @Component({
   selector: 'app-reports',
@@ -7,9 +12,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportsComponent implements OnInit {
 
-  constructor() { }
+  private purcahseReportForm: FormGroup;
+  private deleteFlag: boolean = true;
+  private endPoint: string = "reports/";
+  private formSuccess: boolean = false;
+  private formRequiredError: boolean = false;
+  private nameFlag: boolean = false;
+
+  @ViewChild(ButtonsComponent) buttonsComponent: ButtonsComponent;
+
+  constructor(private fb: FormBuilder,
+    private translate: TranslateService,
+    private sharedDataService: SharedDataService,
+    private masterService: MasterService) {
+    translate.setDefaultLang('messages.en');
+
+  }
 
   ngOnInit() {
+    this.purcahseReportForm = this.fb.group({
+      id: ['', Validators.required],
+      number: ['', Validators.required],
+      gsttype:[],
+      fromDate:[],
+      toDate:[],
+      repType:[],
+      custOption:[],
+
+    });
   }
 
 }
