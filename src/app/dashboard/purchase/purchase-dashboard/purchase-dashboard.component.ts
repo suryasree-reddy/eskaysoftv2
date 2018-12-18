b18d44056121abe492dd6e455b4955cb2605dbd4
suryasree-reddy <suryasreereddyavuloori@gmail.com>
2018-12-18 17:09:09 +0530

    chnages

--- src/app/dashboard/purchase/purchase-dashboard/purchase-dashboard.component.ts
+++ src/app/dashboard/purchase/purchase-dashboard/purchase-dashboard.component.ts
@@ -8,7 +8,6 @@ import { MasterService } from 'src/app/dashboard/master/master.service';
 import 'src/assets/styles/mainstyles.scss';
 import { SharedDataService } from 'src/app/shared/model/shared-data.service';
 import { ButtonsComponent } from '../../../commonComponents/buttons/buttons.component';
-// import { ConfirmationModelDialogComponent } from 'src/app/commonComponents/confirmation-model-dialog/confirmation-model-dialog.component';
 import * as _ from 'lodash';
 
 @Component({
@@ -28,15 +27,15 @@ export class PurchaseDashboardComponent implements OnInit {
 
   public purchaseForm: FormGroup;
   private endPoint: string = "purchaseentry/";
+  private formSuccess = false;
+  private formRequiredError = false;
+  private nameFlag = false;
   public gridDataList: any = [];
-  public gridColumnNamesList;
-  public gridSelectedRow;
-  public formSuccess: boolean = false;
-  public formRequiredError: boolean = false;
-  public nameFlag;
-  public deleteFlag: boolean =true;
-  public duplicateMessage: string = null;
+  private productsList: any = [];
+  private suppliersList: any = [];
+  private savedSupplierId = 0;
   private modeType: any[];
+  private deleteFlag = true;
 
   @ViewChild('focus') focusField: ElementRef;
   @ViewChild(ButtonsComponent) buttonsComponent: ButtonsComponent;
@@ -59,63 +58,173 @@ export class PurchaseDashboardComponent implements OnInit {
   ngOnInit() {
 
     this.purchaseForm = this.fb.group({
-      id: [],
+      id: ['', Validators.required],
       purchaseNumber: ['', Validators.required],
-      invoiceNumber:[],
+      invoiceNumber:['', Validators.required],
       date: ['', Validators.required],
-      gstin: ['', Validators.required],
+      invoiceDate: ['', Validators.required],
+      accountInformationId: ['', Validators.required],
+      supplier: ['', Validators.required],
+      gstIN: ['', Validators.required],
       wayBill: ['', Validators.required],
       transport: ['', Validators.required],
       numberOfCases: ['', Validators.required],
-      invoiceDate: ['', Validators.required],
+     
       mode: ['', Validators.required],
       lrNumber: ['', Validators.required],
       lrDate: ['', Validators.required],
       delvFrom: ['', Validators.required],
+      productId: ['', Validators.required],
       productCode: ['', Validators.required],
       productName: ['', Validators.required],
-      batch:[],
-      expiry:[],
-      qty:[],
+      batch:['', Validators.required],
+      expiry:['', Validators.required],
+      qty:['', Validators.required],
     
-      othCharges:[],
-      grsValue:[],
-      discount:[],
-      ptd:[],
-      saleRate:[],
-      tax:[],
-      hsn:[],
-      mrp:[],
-      mfgName:[],
-      purRate:[],
-      free:[],
-      grossValue:[],
-      discountValue:[],
-      taxValue:[],
-      netValue:[],
-      debitAdjustmentLedger:[],
-      CreditAdjustmentLedger:[],
-      remarks:[],
-      DebitAdjustmentValue:[],
-      creditAdjustmentValue:[],
-      invoiceValue:[],
 
-      gstPercent:[],
-      taxable:[],
-      cgstAmt:[],
-      sgstAmt:[],
-      inGstPercent:[],
-      inTaxable:[],
-      inCgstAmt:[],
-      inSgstAmt:[],
+
+      othCharges:['', Validators.required],
+      grsValue:['', Validators.required],
+      discount:['', Validators.required],
+      ptd:['', Validators.required],
+      saleRate:['', Validators.required],
+      tax:['', Validators.required],
+      hsn:['', Validators.required],
+      mrp:['', Validators.required],
+      mfgName:['', Validators.required],
+      purRate:['', Validators.required],
+      free:['', Validators.required],
+      grossValue:['', Validators.required],
+      discountValue:['', Validators.required],
+      taxValue:['', Validators.required],
+      netValue:['', Validators.required],
+      debitAdjustmentLedger:['', Validators.required],
+      CreditAdjustmentLedger:['', Validators.required],
+      remarks:['', Validators.required],
+      DebitAdjustmentValue:['', Validators.required],
+      creditAdjustmentValue:['', Validators.required],
+      invoiceValue:['', Validators.required],
+
+      gstPercent:['', Validators.required],
+      taxable:['', Validators.required],
+      cgstAmt:['', Validators.required],
+      sgstAmt:['', Validators.required],
+      inGstPercent:['', Validators.required],
+      inTaxable:['', Validators.required],
+      inCgstAmt:['', Validators.required],
+      inSgstAmt:['', Validators.required],
 
     });
-    // this.loadGridData();
-    // this.getGridCloumsList();
+    this.loadProductData();
+    this.loadSupplierData();
      this.focusField.nativeElement.focus();
      this.modeType = this.sharedDataService.getSharedCommonJsonData().Mode;
   }
-  openModal(template: TemplateRef<any>) {
-    this.modalRef = this.modalService.show(template, { class: 'modal-md' });
+  onInitialDataLoad(dataList: any[]) {
+    this.gridDataList = dataList;
+  }
+  loadGridData() {
+    this.masterService.getData(this.endPoint);
+    this.masterService.dataObject.subscribe(list => {
+      this.gridDataList = list;
+      localStorage.setItem('rowDataLength', JSON.stringify(this.gridDataList.length));
+    });
+  }
+
+  valueChange(selectedRow: any[]): void {
+    this.editable(selectedRow);
+  }
+
+  loadProductData() {
+    this.masterService.getParentData('product/').subscribe(list => {
+      this.productsList = list;
+    });
+  }
+
+  loadSupplierData() {
+    this.masterService.getParentData('accountinformation/').subscribe(list => {
+      this.suppliersList = list;
+    });
+  }
+
+  onSelectProduct(event) {
+    this.purchaseForm.patchValue({ pack: event.item.packing });
+    this.purchaseForm.patchValue({ free: event.item.free });
+    this.purchaseForm.patchValue({ productBoxPack: event.item.boxQty });
+    this.purchaseForm.patchValue({ productId: event.item.id });
+    this.purchaseForm.patchValue({ productcode: event.item.productcode });
+    this.purchaseForm.patchValue({ netRate: event.item.netRate });
+    this.calculateRate();
+  }
+
+  calculateRate() {
+    this.purchaseForm.patchValue({ purRate: this.purchaseForm.value.qty * this.purchaseForm.value.netRate });
+    this.purchaseForm.patchValue({ bQty: this.purchaseForm.value.qty / this.purchaseForm.value.productBoxPack });
+    this.purchaseForm.patchValue({ taxable: this.purchaseForm.value.grossValue -  this.purchaseForm.value.discountValue });
+    this.purchaseForm.patchValue({ bFree: this.purchaseForm.value.bQty * this.purchaseForm.value.free });
+    this.purchaseForm.patchValue({ grossValue: this.purchaseForm.value.qty * this.purchaseForm.value.netRate });
+  }
+
+  onSelectSupplier(event) {
+    if (this.savedSupplierId >= 0 && this.savedSupplierId !== event.item.id) {
+      this.purchaseForm.patchValue({ accountInformationId: event.item.id });
+      this.purchaseForm.patchValue({ gstIN: event.item.gstIN });
+      this.purchaseForm.patchValue({ hsn: event.item.hsnCode });
+      this.purchaseForm.patchValue({ purchaseNumber: this.gridDataList.length + 1 });
+    }
+  }
+
+  save() {
+    this.savedSupplierId = this.purchaseForm.value.accountInformationId;
+    this.buttonsComponent.save();
+  }
+
+  delete() {
+    this.buttonsComponent.delete();
+  }
+
+  deleteOrder() {
+    this.buttonsComponent.manualDelete(this.endPoint + 'purchaseNumber/', this.purchaseForm.value.purchaseNumber);
+  }
+
+  successMsg() {
+    this.formSuccess = true;
+    this.formRequiredError = false;
+    const tempSupplierId = this.purchaseForm.value.accountInformationId;
+    const tempSupplierName = this.purchaseForm.value.supplier;
+    this.resetForm(null);
+    this.purchaseForm.value.accountInformationId = tempSupplierId;
+    this.purchaseForm.value.supplier = tempSupplierName;
+  //  this.loadGridData();
+  }
+
+  requiredErrMsg() {
+      this.formRequiredError = true;
+      this.formSuccess = false;
+  }
+
+  resetForm(param) {
+    const tempSupplierId = this.purchaseForm.value.accountInformationId;
+    const tempSupplierName = this.purchaseForm.value.supplier;
+    const tempOrderNum = this.purchaseForm.value.purchaseNumber;
+    this.purchaseForm.reset();
+    if ((param === undefined || param === null ) && !this.nameFlag) {
+      this.purchaseForm.patchValue({ accountInformationId: tempSupplierId });
+      this.purchaseForm.patchValue({ supplier: tempSupplierName });
+      this.purchaseForm.patchValue({ purchaseNumber: tempOrderNum });
+    }
+    this.deleteFlag = true;
+    this.nameFlag = false;
+    this.formRequiredError = this.formSuccess = false;
+    this.loadGridData();
+  }
+
+  editable(s) {
+    this.nameFlag = true;
+    this.formRequiredError = false;
+    this.deleteFlag = false;
+    this.purchaseForm.reset(s);
+    const productObj = _.find(this.productsList, function(o) {return o.id === s.productId; });
+    this.onSelectProduct({item : productObj});
   }
 }
