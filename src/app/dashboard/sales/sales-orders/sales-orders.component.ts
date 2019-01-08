7e29e3d57e25160bee76d94450766d98ffa6ff75
suryasree-reddy <suryasreereddyavuloori@gmail.com>
2019-01-08 10:52:59 +0530

    sales-order

--- src/app/dashboard/sales/sales-orders/sales-orders.component.ts
+++ src/app/dashboard/sales/sales-orders/sales-orders.component.ts
@@ -26,6 +26,7 @@ export class SalesOrdersComponent implements OnInit {
   private customersList: any = [];
   private savedSupplierId = 0;
   private typeList: any[];
+  private totalValue;
 
   @ViewChild('focus') focusField: ElementRef;
   @ViewChild(ButtonsComponent) buttonsComponent: ButtonsComponent;
@@ -40,12 +41,13 @@ export class SalesOrdersComponent implements OnInit {
   ngOnInit() {
     this.salesOrderForm = this.fb.group({
       id: [],
-      accountInformationId: ['', Validators.required],
-      orderNumber: ['', Validators.required],
+      accountInformationId: [],
+      orderNumber: [''],
+      serialNumber:[''],
       customer: ['', Validators.required],
-      remarks: ['', Validators.required],
+      remarks: [],
       date: ['', Validators.required],
-      productId: ['', Validators.required],
+      productId: [],
       productName: ['', Validators.required],
       productcode: ['', Validators.required],
       pack: ['', Validators.required],
@@ -63,12 +65,20 @@ export class SalesOrdersComponent implements OnInit {
 
   onInitialDataLoad(dataList: any[]) {
     this.gridDataList = dataList;
+    this.totalCalculation();
   }
-
+  totalCalculation(){
+    let total = 0;
+    this.gridDataList.forEach(element => { 
+      total += parseInt(element.value);     
+    });
+    this.totalValue = total;
+  }
   loadGridData() {
     this.masterService.getData(this.endPoint);
     this.masterService.dataObject.subscribe(list => {
       this.gridDataList = list;
+      this.totalCalculation();
       localStorage.setItem('rowDataLength', JSON.stringify(this.gridDataList.length));
     });
   }
@@ -92,29 +102,44 @@ export class SalesOrdersComponent implements OnInit {
   onSelectProduct(event) {
     this.salesOrderForm.patchValue({ pack: event.item.packing });
     this.salesOrderForm.patchValue({ free: event.item.free });
-    this.salesOrderForm.patchValue({ productBoxPack: event.item.boxQty });
     this.salesOrderForm.patchValue({ productId: event.item.id });
     this.salesOrderForm.patchValue({ productcode: event.item.productcode });
     this.salesOrderForm.patchValue({ netRate: event.item.netRate });
-    this.calculateRate();
+    this.calculateRate();    
   }
 
   calculateRate() {
     this.salesOrderForm.patchValue({ rate: this.salesOrderForm.value.qty * this.salesOrderForm.value.netRate });
-    this.salesOrderForm.patchValue({ value: this.salesOrderForm.value.rate * this.salesOrderForm.value.qty });
-
-
+   this.salesOrderForm.patchValue({ value: this.salesOrderForm.value.rate * this.salesOrderForm.value.qty });
+   
   }
 
-  onSelectSupplier(event) {
-    if (this.savedSupplierId >= 0 && this.savedSupplierId !== event.item.id) {
-      this.salesOrderForm.patchValue({ accountInformationId: event.item.id });
-      this.salesOrderForm.patchValue({ orderNumber: this.gridDataList.length + 1 });
+  onSelectSupplier(event) {    
+    this.salesOrderForm.patchValue({ accountInformationId: event.item.id });   
+  }
+  generateOrderNo(){
+    if(!this.salesOrderForm.value.orderNumber){
+      if(this.gridDataList && this.gridDataList.length == 0){
+        this.salesOrderForm.patchValue({ orderNumber: 1});
+      }else{
+        let orderN0 = Math.max.apply(Math, this.gridDataList.map(function(o) { return o.orderNumber; }))
+        this.salesOrderForm.patchValue({ orderNumber: orderN0+1});
+      } 
+    }        
+  }
+  generateSerialNo(){
+    let subList = this.gridDataList.filter(v => v.orderNumber === this.salesOrderForm.value.orderNumber)
+    if(subList && subList.length == 0){
+      this.salesOrderForm.patchValue({ serialNumber: 1});
+    }else{
+      let serialN0 = Math.max.apply(Math, subList.map(function(o) { return o.serialNumber; }))
+      this.salesOrderForm.patchValue({ serialNumber: serialN0+1});
     }
   }
-
   save() {
-    this.savedSupplierId = this.salesOrderForm.value.accountInformationId;
+    this.savedSupplierId = this.salesOrderForm.value.accountInformationId;     
+    this.generateOrderNo(); 
+    this.generateSerialNo();     
     this.buttonsComponent.save();
   }
 
@@ -123,34 +148,37 @@ export class SalesOrdersComponent implements OnInit {
   }
 
   deleteOrder() {
-    this.buttonsComponent.manualDelete(this.endPoint + '/orderNumber', this.salesOrderForm.value.orderNumber);
+    this.buttonsComponent.manualDelete(this.endPoint + 'orderNumber/', this.salesOrderForm.value.orderNumber);
   }
 
   successMsg() {
     this.formSuccess = true;
     this.formRequiredError = false;
     const tempSupplierId = this.salesOrderForm.value.accountInformationId;
-    const tempSupplierName = this.salesOrderForm.value.supplier;
+    const tempSupplierName = this.salesOrderForm.value.customer;
     this.resetForm(null);
     this.salesOrderForm.value.accountInformationId = tempSupplierId;
-    this.salesOrderForm.value.supplier = tempSupplierName;
-    //  this.loadGridData();
-  }
+    this.salesOrderForm.value.customer = tempSupplierName;
+    }
 
   requiredErrMsg() {
-    this.formRequiredError = true;
-    this.formSuccess = false;
+      this.formRequiredError = true;
+      this.formSuccess = false;
   }
 
   resetForm(param) {
     const tempSupplierId = this.salesOrderForm.value.accountInformationId;
-    const tempSupplierName = this.salesOrderForm.value.supplier;
+    const tempSupplierName = this.salesOrderForm.value.customer;
     const tempOrderNum = this.salesOrderForm.value.orderNumber;
+    const tempDate = this.salesOrderForm.value.date;
+    const tempRemarks = this.salesOrderForm.value.remarks;
     this.salesOrderForm.reset();
-    if ((param === undefined || param === null) && !this.nameFlag) {
+    if ((param === undefined || param === null ) && !this.nameFlag) {
       this.salesOrderForm.patchValue({ accountInformationId: tempSupplierId });
-      this.salesOrderForm.patchValue({ supplier: tempSupplierName });
+      this.salesOrderForm.patchValue({ customer: tempSupplierName });
       this.salesOrderForm.patchValue({ orderNumber: tempOrderNum });
+      this.salesOrderForm.patchValue({ date: tempDate });
+      this.salesOrderForm.patchValue({ remarks: tempRemarks });
     }
     this.deleteFlag = true;
     this.nameFlag = false;
@@ -163,7 +191,7 @@ export class SalesOrdersComponent implements OnInit {
     this.formRequiredError = false;
     this.deleteFlag = false;
     this.salesOrderForm.reset(s);
-    const productObj = _.find(this.productsList, function (o) { return o.id === s.productId; });
-    this.onSelectProduct({ item: productObj });
+    const productObj = _.find(this.productsList, function(o) {return o.id === s.productId; });
+    this.onSelectProduct({item : productObj});
   }
 }
